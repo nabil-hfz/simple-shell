4cdfbf92f85d0de73d4893916481170f2db4fcfc
Alhafez nabil <nabil.alhafez@roaa.tech>
2023-01-16 16:19:55 +0200

    # Initial Commit - Adding basic features:
    # A Simple C shell that is able to create, move, delete, list and use wild-card feature on files and folders.
    # Deleting unrelated file

--- main.cpp
+++ main.cpp
@@ -1,19 +1,3 @@
-//#include <iostream>
-//
-//int main() {
-//    std::cout << "Hello, World!" << std::endl;
-//    return 0;
-//}
-//– Implemented a system function to provide data (PID, status, statistics related to execution, etc.)
-//about all descendants of a given process (in DFS order). Write programs in userland to display the resulting trees, using the new system function.
-
-
-
-
-
-//To implement a system function to provide data about all descendants of a given process, you could use the procfs file system, which provides a view of the system's process information in the form of a file hierarchy. You can access the information about a specific process by reading the files in the corresponding directory under /proc.
-//
-//The following is an example of how the system function could be implemented in C:
 
 #include <dirent.h>
 #include <stdio.h>
@@ -57,10 +41,3 @@ void print_tree(pid_t pid, int level) {
     closedir(dir);
 }
 
-
-//This function takes the pid of the process and the level of the current process and recursively iterates over all its child processes directory, prints their PIDs and then call the function again with the child process pid and level + 1 as arguments.
-//
-//You can use this function by calling print_tree(pid, 0); with the desired process pid.
-//This function uses the DFS algorithm, so it will iterate over all descendant of the given process in a depth first search order and print their pid in the tree format.
-//
-//You can also gather more information about the process status and statistics by reading files under the proc directory like /proc/[pid]/status or /proc/[pid]/stat , and parse them accordingly to extract the needed
